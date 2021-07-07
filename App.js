f58033577f2e9383974363956e1115a9f0e5a741
ifelsegrandmaster <patricechaula@gwerupoly.ac.zw>
2021-07-08 01:40:49 +0200

    Configure EAS Build for Android

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import "react-native-gesture-handler";
+import { Provider as PaperProvider } from 'react-native-paper';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import ExchangeRateHistory from "./components/exchangeratehistory/ExchangeRateHistory";
+import Calculator from "./components/calculator/Calculator";
+import PreviousQueries from "./components/previousqueries/PreviousQueries";
+import NavigationBar from "./components/NavigationBar";
+const Stack  = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Calculator"
+          screenOptions={{
+            header: (props) => <NavigationBar {...props} />
+          }}
+        >
+          <Stack.Screen name="Calculator" component={Calculator} />
+          <Stack.Screen name="ExchangeRateHistory" component={ExchangeRateHistory} />
+          <Stack.Screen name="PreviousQueries" component={PreviousQueries} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
